1219116badb13af1a326adb1e8025ba233ab24ef
Alexey Vorobiev <alexey.worobiew@gmail.com>
2024-10-14 23:02:16 +0300

    experimental try gensin

--- words.py
+++ words.py
@@ -23,7 +23,46 @@ for key, value in dict(words_dict).items():
 # print(f"type(dict(words_dict).items(){type(dict(words_dict).items())}")
 
 x = lambda x: x[1]
+print(x)
 sorted1 = sorted(words_dict.items(), key=x)
-print(sorted1)
+# print(sorted1)
 stripped_text = text.strip(' ')
 file.close()
+
+
+'''
+try gensin
+'''
+from gensim.summarization.summarizer import summarize
+from gensim.summarization import keywords
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import nltk
+nltk.download('stopwords')
+
+# Set Russian stopwords
+stopWords = set(stopwords.words('russian'))
+
+# Sample Russian text
+text = "Ваш текст здесь"
+
+# Preprocess the text (tokenize and remove stopwords)
+words = word_tokenize(text.lower())
+words = [word for word in words if word not in stopWords]
+
+# Join the words back into a string
+text = ' '.join(words)
+
+# Summarize the text
+summary = summarize(text, ratio=0.3)
+
+# Get keywords
+key_words = keywords(text).split('\n')
+
+print("Оригинальный текст:")
+print(text)
+print("\nСводка:")
+print(summary)
+print("\nКлючевые слова:")
+for word in key_words:
+    print(word)
